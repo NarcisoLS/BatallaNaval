ddf045565212b20c577a5934e93dfae6f859880a
Narciso Lopez Secundino <93229832+NarcisoLS@users.noreply.github.com>
2021-11-25 09:44:04 -0600

    Add files via upload

---
+++ BatallaNaval.js
@@ -0,0 +1,135 @@
+=======
+    //crear una instancia de la clase jugador
+    Jugador jugador = nuevo jugador();
+    //Asignarle un nombre al jugador
+    Jugador.setNombre("Narciso");
+    //Crear una instancia de la clase barco
+    Barco barco = Nuevo barco();
+    //Asignarle un nombre al barco
+    Barco.setNombre("Barco");
+    //Crear
+
+=======
+    //crear una instancia de la clase jugador
+    Jugador jugador = nuevo jugador();
+    //Asignarle un nombre al jugador
+    Jugador.setNombre("Abraham");
+    //Crear una instancia de la clase barco
+    Barco barco = Nuevo barco();
+    //Asignarle un nombre al barco
+    Barco.setNombre("Barco");
+    //Crear
+
+
+
+=======
+
+<!-- Path: index2.html -->
+<html>
+<head>
+<title>Jugar</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+</head>
+<body id="top">
+<div class="wrapper row0">
+  <div id="topbar" class="clear"> 
+    <div class="fl_left">
+      <ul class="nospace linklist">
+        <li><a href="#">Link 1</a></li>
+        <li><a href="#">Link 2</a></li>
+        <li><a href="#">Link 3</a></li>
+        <li><a href="#">Link 4</a></li>
+        <li><a href="#">Link 5</a></li>
+      </ul>
+    </div>
+    <div class="fl_right">
+      <ul class="nospace linklist">
+        <li><i class="fa fa-phone"></i> +00 (123) 456 7890</li>
+        <li><i class="fa fa-envelope-o"></i>
+
+=======
+
+</script> 
+<body>
+    <div id="wrapper">
+        <div id="header">
+            <h1> <a href="index2.html">Jugar</a> </h1>
+        </div>
+        <div id="menu">
+            <ul>
+                <li><a href="index2.html">Inicio</a></li>
+                <li><a href="index2.html">Acerca de</a></li>
+                <li><a href="index2.html">Contactos</a></li>
+                <li><a href="index2.html">Ayuda</a></li>
+            </ul>
+        </div>
+        <div id="content">
+            <div id="colOne">
+                <div class="box">
+                    <h2>Bienvenido</h2>
+                    <ul>
+                        <li>
+                            <h3>Nombre:</h3>
+                            <p>Narciso</p>
+                        </li>
+                        <li>
+                            <h3>Fecha de Nacimiento:</h3>
+                            <p>12/12/12</p>
+                        </li>
+                        <li>
+                            <h3>Direccion:</h3>
+                            <p>Calle #123</p>
+                        </li>
+                        <li>
+
+=======
+
+</script> 
+<body>
+<h1>
+<span class="badge badge-secondary">
+    <span class="badge badge-secondary">
+        <span class="badge badge-secondary">
+            <span class="badge badge-secondary">
+                <span class="badge badge-secondary">
+                    <span class="badge badge-secondary">
+                        <span class="badge badge-secondary">
+                            <span class="badge badge-secondary">
+                                <span class="badge badge-secondary">
+                                    <span class="badge badge-secondary">
+                                        <span class="badge badge-secondary">
+                                            <span class="badge badge-secondary">
+                                                <span class="badge badge-secondary">
+                                                    <span class="badge badge-secondary">
+                                                        <span class="badge badge-secondary">
+                                                            <span class="badge badge-secondary">
+                                                                <span class="badge badge-secondary">
+                                                                    <span class="badge badge-secondary">
+                                                                        <span class="badge badge-secondary">
+                                                                            <span class="badge badge-secondary">
+                                                                                <span class="badge badge-secondary">
+                                                                                    <span class="badge badge-secondary">
+                                                                                        <span class="badge badge-secondary">
+
+=======
+
+</script> 
+
+<body>
+<!--
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-6">
+        <h1>TableroU1</h1>
+        <div id="tableroU1" class="tableroU1"></div>
+      </div>
+      <div class="col-sm-6">
+        <h1>TableroU1Disparos</h1>
+        <div id="tableroU1Disparos" class="tableroU1Disparos"></div>
+      </div>
+    </div>
+  </div>
+-->
+</body>
